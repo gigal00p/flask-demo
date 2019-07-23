414693f5f497cce120e3eb20209602b8ca6abb71
Krzysztof Walkiewicz <krzysztof.walkiewicz@roche.com>
2019-07-23 15:46:45 +0200

    Dummy change

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Terraform is so cool!</h3>"
+    html = "<h3>IAC is so cool!</h3>"
     return html
 
 if __name__ == "__main__":
