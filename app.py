2fc6ee9635039d76c140a741665bb540da02d064
Krzysztof Walkiewicz <krzysztof.walkiewicz@roche.com>
2020-04-02 12:37:59 +0200

    New h1 tag

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h1>DataOps team rocks!</h1>"
+    html = "<h1>Terraform rocks!</h1>"
     return html
 
 if __name__ == "__main__":
